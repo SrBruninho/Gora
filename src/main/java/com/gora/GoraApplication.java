b15d11589585ab7f127896a7817c1f4314a4d3fb
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-07-21 23:33:02 -0300

    Initial Commit

---
+++ src/main/java/com/gora/GoraApplication.java
@@ -0,0 +1,13 @@
+package com.gora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gora/GoraApplicationTests.java
@@ -0,0 +1,18 @@
+package com.gora;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoraApplicationTests {
+
+	@Test
+	void test1() {
+		
+		assertEquals(1, 1);
+	}
+
+}
